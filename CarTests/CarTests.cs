d06550d36d90777177b5fe37310a09888e614618
Alaina Balducci <alainabalducci@gmail.com>
2023-03-14 10:23:53 -0500

    completed unit testing exercise

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CarNS;
+using System;
 
 namespace CarTests
 {
@@ -27,6 +28,28 @@ namespace CarTests
         {
             Assert.AreEqual(10, test_car.GasTankLevel, .001);
         }
+
+        [TestMethod]
+        public void TestGasTankAfterDriving() 
+        {
+            test_car.Drive(50);
+            Assert.AreEqual(9, test_car.GasTankLevel, .001);
+        }
+        [TestMethod]
+        public void TestGasTankAfterExceedingTankRange()
+        {
+            test_car.Drive(501);
+            Assert.AreEqual(test_car.GasTankLevel, 0, .001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGasOverfillException()
+        {
+            test_car.AddGas(5);
+            Assert.Fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+        }
+
 
         //TODO: gasTankLevel is accurate after driving within tank range
         //TODO: gasTankLevel is accurate after attempting to drive past tank range
